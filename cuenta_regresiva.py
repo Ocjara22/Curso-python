def38bb3a069b5cace2490fa1b4ea99d73cdcb6b
Ocjara22 <ocjara22@gmail.com>
2019-04-25 00:18:29 -0300

    mejoras

--- cuenta_regresiva.py
+++ cuenta_regresiva.py
@@ -1,10 +1,10 @@
 # ---------------------------------------------EJERCICIO 2 cuenta_regresiva.py
 
 
-cuenta_regresiva = int(input("Ingrese un número para comenzar la cuenta\n"))
-for i in range(cuenta_regresiva):
-    tmp = cuenta_regresiva
-    print("Iteración {}".format(tmp - i))
+# cuenta_regresiva = int(input("Ingrese un número para comenzar la cuenta\n"))
+# for i in range(cuenta_regresiva):
+#     tmp = cuenta_regresiva
+#     print("Iteración {}".format(tmp - i))
 
 
 cuenta_regresiva = int(input("Ingrese un número para comenzar la cuenta\n"))
--- iterador.py
+++ iterador.py
@@ -1,5 +1,8 @@
 #------------------------------------ EJERCICIO 1
-i = 0
-while i < 50:
+# i = 0
+# while i < 50:
+#     print("Iteración {}".format(i + 1))
+#     i +=1
+
+for i in range(0,50) :
     print("Iteración {}".format(i + 1))
-    i +=1
